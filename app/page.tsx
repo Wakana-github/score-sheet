802af4063431ce7ecd3cf5528bd2d90c969e6c8c
wakana-github <wakana_devwork@proton.me>
2025-10-02 12:06:25 +1000

    Implemented React-Select dropdown to Home page. Added styles for a lerger screen to Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/clerk-react';
+import Select from 'react-select'; 
 
 
 interface GameData {
@@ -21,7 +22,7 @@ export default function Home() {
   const [selectedGameId, setSelectedGameId] = useState<number | null>(null);
   const router = useRouter();
 
-  // Fetch game data fron API
+  // Fetch game data from API
   useEffect(() => {
     const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
     fetch(`${baseUrl}/games`)
@@ -33,10 +34,8 @@ export default function Home() {
   }, []); // [] <- render once
 
   // handle game titles
-  const handleGameChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = event.target.value;
-    // set selected game ID. set null when the value is empty.
-    setSelectedGameId(value === '' ? null : Number(value));
+  const handleGameChange = (selectedOption: { value: number, label: string } | null) => {
+     setSelectedGameId(selectedOption ? selectedOption.value : null);
   };
 
   // Go To Score Sheet button handler
@@ -72,62 +71,58 @@ export default function Home() {
     );
   }
   const sortedGames = [...games].sort((a, b) => a.title.localeCompare(b.title));
+  
+  //Alter sortedGame options to react-select options for the drop box
+  const gameOptions = sortedGames.map(game => ({
+     value: game.id, 
+     label: game.title,
+  }));
+
+
   return (
     <main>
       <div className="flex items-center justify-center h-screen">
         <div className="text-center">
-          <p className="hand_font text-3xl pt-4 ">Welcome to</p>
-          <h1 className=" hand_font text-5xl font-bold">Score Sheet</h1>
+          <p className="hand_font text-2xl lg:text-4xl pt-4 ">Welcome to</p>
+          <h1 className=" hand_font text-4xl lg:text-6xl font-bold">Score Sheet</h1>
 
           {/* game select form */}
-          <div className="dark_green py-2 my-6 px-3">
-            <label htmlFor="game" className="hand_font text-3xl">Choose a game: </label>
-            <select
+          <div className="dark_green py-1 lg:py-4 my-3 lg:my-6 px-3 flex items-center">
+            <label htmlFor="game" className="hand_font text-2xl lg:text-4xl">Choose a game: </label>
+            <Select
               id="game"
               name="gamelist"
               form="gameform"
-              value={selectedGameId === null ? '' : selectedGameId} // set null when the game hass not selected
+              value={gameOptions.find(option => option.value === selectedGameId) || null}// set null when the game hass not selected
               onChange={handleGameChange}
-              className=" p-2
-              rounded-md
-              focus:outline-none
-              focus:ring-1 focus:dark_green
-              outline-none
-              border-none
-              ring-0
-              shadow-none"
-            >
-              {/* reset the selected game value */}
-              <option value="">Choose the game</option>
-             {sortedGames.map((game) => (
-                <option key={game.id} value={game.id}>
-                  {game.title}
-                </option>
-              ))}
-            </select>
+              className="text-sm md:text-base lg:text-xl py-2 ml-2" 
+              placeholder="Choose game"
+              options={gameOptions}
+              isSearchable={true}
+            />
           </div>
 
           {/* Custom sheet/Score sheet button */}
           <button
             // call handleGoToCustomSheet when selectedGameId = null、otherwise call handleGoToScoreSheet
             onClick={selectedGameId === null ? handleGoToCustomSheet : handleGoToScoreSheet}
-            className="dark_green py-2 my-2 w-64"
+            className="dark_green py-1 lg:py-4 px-6 lg:px-8"
           >
-            <span className="hand_font text-3xl">
+            <span className="hand_font text-2xl lg:text-4xl">
               {/* display Custom Sheet when selectedGameId = nul、otherwise display Go to the Score Sheet */}
               {selectedGameId === null ? 'Custom Sheet' : 'Go to the Score Sheet'}
             </span>
           </button>
 
           {/* my record button */}
-          <div className="my-4">
-            <button onClick={handleViewRecords} className="hand_font text-3xl">
+          <div className="mt-3 lg:my-8">
+            <button onClick={handleViewRecords} className="hand_font text-2xl lg:text-4xl">
             My Record
           </button>
           </div>
 
           {/* login / signin button */}
-         <div className=" my-6 text-xl flex justify-center items-center gap-4"> {/* flex, justify-center, items-center, gap-4 を追加してボタンを中央に並べる */}
+         <div className="my-2 lg:my-4 flex justify-center items-center gap-4 text-lg lg:text-2xl"> {/* flex, justify-center, items-center, gap-4 を追加してボタンを中央に並べる */}
             <SignedOut>
               {/* Sign-in button*/}
               <SignInButton
@@ -135,19 +130,19 @@ export default function Home() {
               >
                 {/* log in button */}
                 <button className="bg-transparent border-none  cursor-pointer p-0">
-                  <span className=" hand_font text-xl underline">log in</span>
+                  <span className=" hand_font underline">log in</span>
                 </button>
               </SignInButton>
 
               {/* Separator */}
-              <span className="text-xl"> / </span>
+              <span> / </span>
 
               {/* Sign-up button */}
               {/* Opens the sign-up form in a modal */}
               <SignUpButton mode="modal">
                 {/* sign up button */}
                 <button className="bg-[#a81010] rounded-full border-none  cursor-pointer px-3 py-1"> 
-                  <span className="text-white hand_font text-xl">sign up</span>
+                  <span className="text-white hand_font">sign up</span>
                 </button>
               </SignUpButton>
             </SignedOut>
